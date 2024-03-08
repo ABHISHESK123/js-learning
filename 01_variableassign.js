53839b14f8212add4dece9f1525e6fa8580eb361
ABHISHESK123 <abhishekshewale9992@gmail.com>
2024-03-08 13:13:33 +0530

    Variable js
    Variables js

---
+++ 01_variableassign.js
@@ -0,0 +1,70 @@
+firstName = "Abhishek Shewale";
+console.log("Name:", firstName);
+
+console.log("______________________");
+
+firstName = "Abhishek Deelip Shewale";
+console.log("FullName:", firstName);
+
+console.log("______________________");
+
+console.log("i love only js");
+
+console.log("______________________");
+
+firstName = "Abhishek Deelip Shewale";
+
+console.log("First Name:", firstName);
+
+LastName = "Shewale";
+console.log("Last Name:", LastName);
+
+age = "10";
+console.log("Age:", age);
+
+Address = "Kolhapur";
+console.log("Address:", Address);
+
+Pin = "416006";
+console.log("Pin:", Pin);
+
+Gender = "Male";
+console.log("Gender:", Gender);
+
+console.log("______________________");
+
+console.log("Before Swap");
+console.log("______________________");
+Sweety = "Sweety";
+console.log("Sweety:", Sweety);
+Cutie = "Cutie";
+console.log("Cutie:", Cutie);
+console.log("______________________");
+
+console.log("After Swap");
+console.log("______________________");
+Sweety = "cuite";
+console.log("Sweety:", Sweety);
+Cutie = "Sweety";
+console.log("Cutie:", Cutie);
+console.log("______________________");
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+console.log("Before swapping:");
+console.log("a =", a);
+console.log("b =", b);
+console.log("c =", c);
+
+// Swapping logic
+let temp = a;
+a = b;
+b = c;
+c = temp;
+
+console.log("After swapping:");
+console.log("a =", a);
+console.log("b =", b);
+console.log("c =", c);
