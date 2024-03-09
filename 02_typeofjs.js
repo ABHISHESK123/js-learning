3573d7275c1c3a31c2b3294613ca2bf9c8b84984
ABHISHESK123 <abhishekshewale9992@gmail.com>
2024-03-09 08:59:09 +0530

    Types Of js
    Types Of js

---
+++ 02_typeofjs.js
@@ -0,0 +1,30 @@
+let bankName;
+console.log(`%cBefore initialization: ${bankName}, Type: ${typeof bankName}`, "color: blue");
+bankName = "SBI Bank";
+console.log(`%cAfter initialization: ${bankName}, Type: ${typeof bankName}`, "color: green");
+
+console.log("___________________________________________________");
+
+let marks = "90%";
+console.log(`%cValue: ${marks}, Type: ${typeof marks}`, "color: orange",);
+
+console.log("___________________________________________________");
+
+let isWorking;
+console.log(`%cBefore initialization: ${isWorking}, Type: ${typeof isWorking}`, "color: blue");
+
+console.log("___________________________________________________");
+isWorking = true;
+console.log(`%cAfter initialization: ${isWorking}, Type: ${typeof isWorking}`, "color: green");
+
+console.log("___________________________________________________");
+
+let totalCount = "Hundred and Seven";
+console.log(`%ca. Value: ${totalCount}, Type: ${typeof totalCount}`, "color: purple");
+
+totalCount = 107;
+console.log(`%cb. Value: ${totalCount}, Type: ${typeof totalCount}`, "color: brown");
+
+console.log("___________________________________________________");
+
+
